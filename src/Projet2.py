9c4e49a43b63348101e7725520747c74019a16ee
LEJ <121897571+DevelopedByLEJ@users.noreply.github.com>
2025-02-04 11:31:07 +0100

    Commit Img

--- src/Projet2.py
+++ src/Projet2.py
@@ -1,6 +1,16 @@
 import cv2 as cv
 import mediapipe as mp
 
+# Charger les images associées aux doigts levés
+images = [
+    cv.imread("C:\IUT\BUTInfo3\Semestre2\App_Graphique\TP_Mediapipe\HandsTracking\imgs\ZeroFinger.png"),  # 0 doigt levé
+    cv.imread("C:\IUT\BUTInfo3\Semestre2\App_Graphique\TP_Mediapipe\HandsTracking\imgs\OneFingers.png"),  # 1 doigt levé
+    cv.imread("C:\IUT\BUTInfo3\Semestre2\App_Graphique\TP_Mediapipe\HandsTracking\imgs\TwoFingers.png"),  # 2 doigts levés
+    cv.imread("C:\IUT\BUTInfo3\Semestre2\App_Graphique\TP_Mediapipe\HandsTracking\imgs\ThreeFingers.png"),  # 3 doigts levés
+    cv.imread("C:\IUT\BUTInfo3\Semestre2\App_Graphique\TP_Mediapipe\HandsTracking\imgs\FourFingers.png"),  # 4 doigts levés
+    cv.imread("C:\IUT\BUTInfo3\Semestre2\App_Graphique\TP_Mediapipe\HandsTracking\imgs\FiveFingers.png")   # 5 doigts levés
+]
+
 cap = cv.VideoCapture(0)
 mpHands = mp.solutions.hands
 hands = mpHands.Hands()
@@ -8,15 +18,17 @@ mpDraw = mp.solutions.drawing_utils
 
 while True:
     success, img = cap.read()
-    img = cv.flip(img, 1)  # Miroir pour un affichage intuitif
+    img = cv.flip(img, 1)  # Effet miroir pour un affichage intuitif
     imgRGB = cv.cvtColor(img, cv.COLOR_BGR2RGB)
     results = hands.process(imgRGB)
+
+    total_fingers = 0
 
     if results.multi_hand_landmarks:
         for handLms in results.multi_hand_landmarks:
             mpDraw.draw_landmarks(img, handLms, mpHands.HAND_CONNECTIONS)
 
-            # Récupération des coordonnées des doigts
+            # Stocker les coordonnées des landmarks
             landmarks = {}
             h, w, _ = img.shape
 
@@ -27,22 +39,33 @@ while True:
             if len(landmarks) >= 21:
                 fingers = []
 
-                # Pouce : Comparaison en X
+                # Pouce (comparaison en X)
                 if landmarks[4][0] > landmarks[2][0]:  # Main droite
                     fingers.append(1)
                 else:
                     fingers.append(0)
 
-                # Autres doigts : Comparaison en Y
+                # Autres doigts (comparaison en Y)
                 for tip, pip in [(8, 6), (12, 10), (16, 14), (20, 18)]:
                     if landmarks[tip][1] < landmarks[pip][1]:  # Doigt levé
                         fingers.append(1)
                     else:
                         fingers.append(0)
 
-                # Nombre de doigts levés
+                # Nombre total de doigts levés
                 total_fingers = sum(fingers)
-                cv.putText(img, f'Fingers: {total_fingers}', (50, 50), cv.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 3)
+
+    # Vérifier que l'index est valide (0 à 5)
+    if 0 <= total_fingers <= 5:
+        overlay = images[total_fingers]  # Sélectionner l'image correspondante
+        if overlay is not None:
+            overlay = cv.resize(overlay, (200, 200))  # Redimensionner
+            img[50:250, 50:250] = overlay  # Afficher en haut à gauche
+        if overlay is None:
+            print("Erreur : Problème lors du chargement de l'image")
+
+    # Affichage du nombre de doigts levés
+    cv.putText(img, f'Fingers: {total_fingers}', (300, 50), cv.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 3)
 
     cv.imshow("Finger Count", img)
     if cv.waitKey(1) == ord('q'):
