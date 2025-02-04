506b884278f6fe06c8535644e1faf3d16be9e086
LEJ <121897571+DevelopedByLEJ@users.noreply.github.com>
2025-02-04 11:56:26 +0100

    Update testMediapipe

--- src/testMediapipe.py
+++ src/testMediapipe.py
@@ -2,8 +2,9 @@ import sys
 
 try:
     import mediapipe as mp
-    print("MediaPipe est bien installé !")
+    print("MediaPipe est bien installé")
     print(f"Version : {mp.__version__}")
 except ImportError:
     print("MediaPipe n'est pas installé. Installe-le avec :")
+    print("pip install mediapipe")
     sys.exit(1)
